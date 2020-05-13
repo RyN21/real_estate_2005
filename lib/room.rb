e2a3a53a40673642f7c128165993b87760168f10
Ryan Laleh <laleh21@yahoo.com>
2020-05-13 11:04:53 -0600

    Update room

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,22 @@
-class Rom
+class Room
+  attr_reader :category, :length, :width, :paint
 
-  def initialize(category, length, width)
+  def initialize(category, length = 10, width)
     @category = category
-    @length = 10
+    @length = length
     @width = width
+    @paint = false
   end
 
   def area
-    @length * @width
+    @length.to_i * @width.to_i
+  end
+
+  def paint
+    @paint = true
+  end
+
+  def is_painted?
+    @paint
   end
 end
